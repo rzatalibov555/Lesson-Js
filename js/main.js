960d2443fe6615cb30d4b4fd414d003053c20966
rzatalibov555 <sethubpro@gmail.com>
2022-09-20 19:43:09 +0400

    outputs

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// document.write("")
+// alert("")
+// prompt("")
+// confirm("")
+// document.getElementsByTagName("DIV")[5].innerHTML = "<p style='color:red'>Hello my tag</p>"
+// document.getElementsByTagName("DIV")[6].innerText = "<p style='color:red'>Hello my tag</p>"
+// console.log()
+// print()
+
+// function pecat(){
+//     print()
+// }
+
+
+
+
+
+// document.write("JS dersi <br>")
+// alert("Gultac")
+// prompt("Enter your username")
+// confirm("Abune olmaq isteyirsinizmi?")
+
+
+// console.log("Men burdayam HAHAHA")
+
+   
+// document.getElementById("demo2").innerHTML = "Esmira muellime"
+// document.getElementById("demo3").innerHTML = "Sebuhi muellim"
+// document.getElementById("demo3").innerHTML = "Sebuhi muellim2"
+
+// document.getElementById("deyanet").innerHTML = "Menim adim Deyanetdir"
+
+// document.getElementsByClassName("demo")[0].innerHTML = "Nese"
+// document.getElementsByClassName("demo")[4].innerHTML = "Nese nebilim nese"
+// document.getElementsByClassName("demo")[2].innerHTML = "Nubar"
+// document.getElementsByClassName("demo")[7].innerHTML = "Nebilim"
+// document.getElementsByClassName("demo")[6].innerHTML = "Panda"
+// document.getElementsByClassName("demo")[5].innerHTML = "Nese nese"
+
+
+// document.getElementsByTagName("div")[0].innerHTML = "Hello Rza"
+// document.getElementsByTagName("DIV")[5].innerHTML = "Xos soz"
+// document.getElementsByTagName("DIV")[7].innerHTML = "Men proqramciyam"
+// document.getElementsByTagName("DIV")[3].innerHTML = "Nubar"
+// document.getElementsByTagName("DIV")[4].innerHTML = "Bax ureyin ne istese"
+// document.getElementsByTagName("DIV")[6].innerHTML = "Hiii"
+
+// // innerHTML
+// // innerText
+// document.getElementsByTagName("DIV")[5].innerHTML = "<p style='color:red'>Hello my tag</p>"
+// document.getElementsByTagName("DIV")[6].innerText = "<p style='color:red'>Hello my tag</p>"
+
+
+
